7f5b3bd9520be1b4a511fa9805484f176a966455
Ethan Torres <ethan@torresonline.com>
2026-01-26 22:25:29 -0500

    Added hood sim and updated shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,12 +1,17 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.MetersPerSecond;
+
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.Supplier;
 
 public class Shooter extends SubsystemBase {
   /** Creates a new Shooter. */
@@ -15,6 +20,9 @@ public class Shooter extends SubsystemBase {
   private double speed;
 
   private final MotionMagicVelocityVoltage shooterVelocity = new MotionMagicVelocityVoltage(0);
+
+  private LinearVelocity goalSpeed = MetersPerSecond.of(0);
+  private LinearVelocity currentSpeed = MetersPerSecond.of(0);
 
   public Shooter() {
     TalonFXConfiguration config = new TalonFXConfiguration();
@@ -32,16 +40,35 @@ public class Shooter extends SubsystemBase {
     shooterMotor.getConfigurator().apply(config);
   }
 
-  public void setSpeed(double speed) {
-    shooterMotor.setControl(shooterVelocity.withVelocity(speed));
+  public void setSpeed(LinearVelocity speed) {
+    // this should be rotations per second but ill fix later
+    shooterMotor.setControl(shooterVelocity.withVelocity(speed.in(MetersPerSecond)));
+  }
+
+  public LinearVelocity getGoalSpeed() {
+    return goalSpeed;
   }
 
   public void stop() {
     shooterMotor.stopMotor();
   }
 
+  public void setGoalSpeed(LinearVelocity speed) {
+    goalSpeed = speed;
+  }
+
+  public LinearVelocity getExitVelocity() {
+
+    return MetersPerSecond.of(9.353);
+  }
+
   @Override
   public void periodic() {
     SmartDashboard.putNumber("Shooter speed", shooterMotor.get());
+
+    if (currentSpeed != goalSpeed) {
+      currentSpeed = goalSpeed;
+      setSpeed(currentSpeed);
+    }
   }
 }
